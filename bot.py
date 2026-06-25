9eeec3cb921a743b8bee9c30278dbc6de162c828
camilac234a-sketch <camilac234a@gmail.com>
2026-06-25 17:25:27 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+import asyncio
+from telegram.ext import ApplicationBuilder, MessageHandler, filters
+
+async def bienvenida(update, context):
+    await update.message.reply_text(
+    "Hola bebe como estas💦🥴\n\n"
+    "Te ofrezco:\n"
+    "💙 Rica videollamada\n"
+    "💙 Sexting\n"
+    "💙 Pack de fotos y videos\n"
+    "💙 Dick rate\n"
+    "💙 Videos personalizados\n"
+    "💙 Suscripción canal premium\n\n"
+    "Manejo:\n"
+    "❤️ Remitly\n"
+    "❤️ Bizum\n"
+    "❤️ Criptomonedas\n"
+    "❤️ BINANCE\n"
+    "❤️ Transferencias mexicanas\n"
+    "❤️ Transferencias colombianas\n"
+    "❤️ Transferencias chilenas\n\n"
+    "¿De dónde eres? ¿Y qué te interesa?\n\n"
+    "SI NO TIENE DINERO NO ME HABLE.\n"
+    "NO HAGO NADA PRESENCIAL, NO PIERDA EL TIEMPO PREGUNTÁNDOMELO."
+)
+app = ApplicationBuilder().token("8629164387:AAEBrzltZlmuaaGIeoFSdUXZBSivSteJyhA").build()
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, bienvenida))
+app.run_polling()
